f605bba44016b61734478c69d48f5ff5ec1f7f91
renaldysab18 <renaldysab18@gmail.com>
2020-09-19 17:04:33 +0700

    [add] adding layout detail team

--- app/src/main/java/com/redveloper/sportapp/ui/detail/team/DetailTeamActivity.kt
+++ app/src/main/java/com/redveloper/sportapp/ui/detail/team/DetailTeamActivity.kt
@@ -13,7 +13,7 @@ class DetailTeamActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_team)
         setSupportActionBar(findViewById(R.id.toolbar))
-        findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
+        findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = "Renal"
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
