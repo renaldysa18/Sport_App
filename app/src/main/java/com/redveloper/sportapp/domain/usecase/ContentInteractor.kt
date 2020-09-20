e5d996bac9858119341fbd98ecba73418b47ca94
renaldysab18 <renaldysab18@gmail.com>
2020-09-20 20:20:17 +0700

    [fix] fixng content interactor

--- app/src/main/java/com/redveloper/sportapp/domain/usecase/ContentInteractor.kt
+++ app/src/main/java/com/redveloper/sportapp/domain/usecase/ContentInteractor.kt
@@ -1,30 +1,24 @@
 package com.redveloper.sportapp.domain.usecase
 
-import androidx.lifecycle.LiveData
 import com.redveloper.sportapp.domain.model.*
 import com.redveloper.sportapp.domain.repository.RepositoryImpl
 import com.redveloper.sportapp.vo.Resource
+import kotlinx.coroutines.flow.Flow
 
 class ContentInteractor (private val repositoryImpl: RepositoryImpl) : ContentUseCase {
-    override fun getAllCountries(): LiveData<Resource<List<Country>>> = repositoryImpl.getAllCountries()
+    override fun getAllCountries(): Flow<Resource<List<Country>>> = repositoryImpl.getAllCountries()
 
-    override fun getAllLeague(country: String): LiveData<Resource<List<League>>> = repositoryImpl.getAllLeague(country)
+    override fun getAllLeague(country: String): Flow<Resource<List<League>>> = repositoryImpl.getAllLeague(country)
 
-    override fun getAllTeamInLeague(league: String): LiveData<Resource<List<Team>>> = repositoryImpl.getAllTeamInLeague(league)
+    override fun getAllTeamInLeague(league: String): Flow<Resource<List<Team>>> = repositoryImpl.getAllTeamInLeague(league)
 
-    override fun getDetailTeam(idTeam: String): LiveData<Resource<Team>> = repositoryImpl.getDetailTeam(idTeam)
-
-    override fun getAllMatchInLeague(idLeague: String): LiveData<Resource<List<Match>>> = repositoryImpl.getAllMatchInLeague(idLeague)
+    override fun getAllMatchInLeague(idLeague: String): Flow<Resource<List<Match>>> = repositoryImpl.getAllMatchInLeague(idLeague)
 
     override fun getAllClassementInLeague(
         idLeague: String,
         season: String
-    ): LiveData<Resource<List<Classement>>> = repositoryImpl.getAllClassementInLeague(idLeague, season)
+    ): Flow<Resource<List<Classement>>> = repositoryImpl.getAllClassementInLeague(idLeague, season)
 
-    override fun setSelectedLeaegue(league: League) = repositoryImpl.setSelectedLeague(league)
-    override fun getSelectedLeague(): LiveData<League> = repositoryImpl.getSelectedLeague()
-    override fun checkLeagueHasItem(): LiveData<Boolean> = repositoryImpl.checkLeagueHasItem()
-
-    override fun getFavoriteTeam(): LiveData<List<Team>> = repositoryImpl.getFavoriteTeam()
+    override fun getFavoriteTeam(): Flow<List<Team>> = repositoryImpl.getFavoriteTeam()
     override fun setFavoriteTeam(team: Team, state: Boolean) = repositoryImpl.setFavoriteTeam(team, state)
 }
