1e8af1be05b8b90b532e2b1f7ea0ea314b813f6e
renaldysab18 <renaldysab18@gmail.com>
2020-09-20 19:46:46 +0700

    [fix] fixing network bound resource with corouitne

--- app/src/main/java/com/redveloper/sportapp/data/NetworkBoundResource.kt
+++ app/src/main/java/com/redveloper/sportapp/data/NetworkBoundResource.kt
@@ -1,80 +1,44 @@
 package com.redveloper.sportapp.data
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
 import com.redveloper.sportapp.data.source.remote.ApiResponse
-import com.redveloper.sportapp.data.source.remote.StatusResponse
 import com.redveloper.sportapp.utils.AppExecutors
 import com.redveloper.sportapp.vo.Resource
+import kotlinx.coroutines.flow.*
 
-abstract class NetworkBoundResource <ResultType, RequestType>(private val mExecutors: AppExecutors){
+abstract class NetworkBoundResource<ResultType, RequestType>(private val mExecutors: AppExecutors) {
 
-    private val result = MediatorLiveData<Resource<ResultType>>()
-
-    init {
-        result.value = Resource.Loading(null)
-
-        @Suppress("LeakingThis")
-        val dbSource = loadFromDB()
-
-        result.addSource(dbSource) {data ->
-            result.removeSource(dbSource)
-            if (shouldFetch(data)){
-                fetchFromNetwork(dbSource)
-            } else {
-                result.addSource(dbSource) {newData ->
-                    result.value = Resource.Succes(newData)
+    private val result: Flow<Resource<ResultType>> = flow {
+        emit(Resource.Loading())
+        val dbSource = loadFromDB().first()
+        if (shouldFetch(dbSource)) {
+            emit(Resource.Loading())
+            when (val apiResponse = createCall().first()) {
+                is ApiResponse.Success -> {
+                    saveCallResult(apiResponse.data)
+                    emitAll(loadFromDB().map { Resource.Success(it) })
                 }
-            }
-        }
-    }
-
-    protected open fun onFetchFailed(){}
-
-    protected abstract fun loadFromDB() : LiveData<ResultType>
-
-    protected abstract fun shouldFetch(data : ResultType?) : Boolean
-
-    protected abstract fun createCall() : LiveData<ApiResponse<RequestType>>
-
-    protected abstract fun saveCallResult(data: RequestType)
-
-    protected fun fetchFromNetwork(dbSource : LiveData<ResultType>){
-        val apiResponse = createCall()
-        result.addSource(dbSource){newData ->
-            result.value = Resource.Loading(newData)
-        }
-
-        result.addSource(apiResponse){response ->
-            result.removeSource(apiResponse)
-            result.removeSource(dbSource)
-            when(response.status){
-                StatusResponse.SUCCESS -> {
-                    mExecutors.diskIO().execute {
-                        response.body?.let { saveCallResult(it) }
-                        mExecutors.mainThread().execute {
-                            result.addSource(loadFromDB()){newData ->
-                                result.value = Resource.Succes(newData)
-                            }
-                        }
-                    }
+                is ApiResponse.Empty -> {
+                    emitAll(loadFromDB().map { Resource.Success(it) })
                 }
-                StatusResponse.EMPTY -> {
-                    mExecutors.mainThread().execute {
-                        result.addSource(loadFromDB()){newData ->
-                            result.value = Resource.Succes(newData)
-                        }
-                    }
-                }
-                StatusResponse.ERROR -> {
+                is ApiResponse.Error -> {
                     onFetchFailed()
-                    result.addSource(dbSource){newData ->
-                        result.value = Resource.Error(newData, response.message)
-                    }
+                    emit(Resource.Error(apiResponse.errorMessage))
                 }
             }
+        } else {
+            emitAll(loadFromDB().map { Resource.Success(it) })
         }
     }
 
-    fun asLiveData() : LiveData<Resource<ResultType>> = result
+    protected open fun onFetchFailed() {}
+
+    protected abstract fun loadFromDB(): Flow<ResultType>
+
+    protected abstract fun shouldFetch(data: ResultType?): Boolean
+
+    protected abstract suspend fun createCall(): Flow<ApiResponse<RequestType>>
+
+    protected abstract suspend fun saveCallResult(data: RequestType)
+
+    fun asFlow(): Flow<Resource<ResultType>> = result
 }
