3397ed61a34d18f19393df3b00e0cd773e896d66
renaldysab18 <renaldysab18@gmail.com>
2020-09-21 16:14:12 +0700

    [fix] fixing dynamic feature

--- about/src/main/java/com/redveloper/sportapp/about/AboutActivity.kt
+++ about/src/main/java/com/redveloper/sportapp/about/AboutActivity.kt
@@ -2,9 +2,11 @@ package com.redveloper.sportapp.about
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.redveloper.sportapp.about.di.aboutModule
-import com.redveloper.sportapp.core.vo.Resource
 import kotlinx.android.synthetic.main.activity_about.*
 import org.koin.android.ext.android.inject
 import org.koin.core.context.loadKoinModules
@@ -12,17 +14,29 @@ import org.koin.core.context.loadKoinModules
 class AboutActivity : AppCompatActivity() {
 
     val viewModel : AboutViewModel by inject()
+    private lateinit var aboutAdapter: AboutAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
 
+        aboutAdapter = AboutAdapter()
+
         loadKoinModules(aboutModule)
+
+        with(rv_about_favorit_team){
+            layoutManager = LinearLayoutManager(this@AboutActivity, RecyclerView.HORIZONTAL, false)
+            adapter = aboutAdapter
+        }
 
         viewModel.favorit.observe(this, Observer { data ->
             if (data != null){
                 val count = data.size
                 tv_count_favorit.text = "Kamu memfavoritkan $count team"
+
+                aboutAdapter.setItems(data)
+                aboutAdapter.notifyDataSetChanged()
+
             }
         })
     }
---
+++ about/src/main/java/com/redveloper/sportapp/about/AboutAdapter.kt
@@ -0,0 +1,41 @@
+package com.redveloper.sportapp.about
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.redveloper.sportapp.core.domain.model.Team
+import kotlinx.android.synthetic.main.layout_item_favorit.view.*
+
+class AboutAdapter : RecyclerView.Adapter<AboutAdapter.ViewHolder>(){
+    private var items : ArrayList<Team> = ArrayList()
+
+    fun setItems(datas : List<Team>?){
+        if (datas.isNullOrEmpty()) return
+        items.clear()
+        items.addAll(datas)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.layout_item_favorit, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindData(items.get(position))
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        fun bindData(data : Team){
+            with(itemView){
+                Glide.with(context)
+                    .load(data.teamBadge)
+                    .into(imgv_item_favorit_team_about)
+            }
+        }
+    }
+}
